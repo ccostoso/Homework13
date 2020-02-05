620b200eaa39ae64e8e29692f46a45bd4a1e6c84
ccostoso <carlos.costoso@outlook.com>
2020-02-05 18:46:03 -0500

    Enhancements for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 // Set up Express.js app
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Set up Express app to handle parsing
 app.use(express.urlencoded({ extended: true }));
